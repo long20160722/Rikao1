5ad8fb00e76f44a544bb8651008c3cd7a241cbd2
long20160722 <525129484@qq.com>
2017-07-05 17:00:58 +0800

    第一次提交

---
+++ app/src/main/java/songqianlong/bwie/com/rikao1/MainActivity.java
@@ -0,0 +1,14 @@
+package songqianlong.bwie.com.rikao1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
